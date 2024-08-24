65352ed450d251fb88112c8fe7b71ca1a4647203
AymanSuleman <sulemanayman30@gmail.com>
2024-08-24 19:57:23 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import { View, Text , SafeAreaView } from 'react-native'
 import React from 'react'
-// import Stacknavigation from './Navigation/Stacknavigation';
+ import Stacknavigation from './Navigation/Stacknavigation';
 // import Welcome from './Screens/Welcome';
-import Apicalling from './Screens/Apicalling';
+// // import Apicalling from './Screens/Apicalling';
 
    export default function App() {
     return (
       
     // <Stacknavigation/>
-    // <Welcome/>
-    <Apicalling/>
+    <Welcome/>
+    // <Apicalling/>
     );
   }
   
